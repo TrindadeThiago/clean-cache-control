4122eaedf7412baa3816c28184b50b2cae8c76ab
Thiago Trindade <thiago.trindadexd@gmail.com>
2021-04-05 19:17:29 -0300

    change mockPurchases to use faker

---
+++ src/data/tests/mock-purchases.ts
@@ -0,0 +1,16 @@
+import faker from 'faker'
+import { SavePurchases } from "@/domain/useCases";
+
+
+export const mockPurchases = (): Array<SavePurchases.Params> => [
+  {
+    id: faker.datatype.uuid(),
+    date: faker.date.recent(),
+    value: faker.datatype.number()
+  },
+  {
+    id: faker.datatype.uuid(),
+    date: faker.date.recent(),
+    value: faker.datatype.number()
+  }
+]
--- src/data/usecase/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecase/save-purchases/local-save-purchases.spec.ts
@@ -1,4 +1,5 @@
 import { CacheStore } from '@/data/protocols/cache'
+import { mockPurchases } from '@/data/tests'
 import { LocalSavePurchases } from "@/data/usecase"
 import { SavePurchases } from '@/domain/useCases'
 
@@ -30,19 +31,6 @@ class CacheStoreSpy implements CacheStore {
     jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => { throw new Error() })
   }
 }
-
-const mockPurchases = (): Array<SavePurchases.Params> => [
-  {
-    id: '1',
-    date: new Date(),
-    value: 50
-  },
-  {
-    id: '2',
-    date: new Date(),
-    value: 30
-  }
-]
 
 type SutTypes = {
   sut: LocalSavePurchases
